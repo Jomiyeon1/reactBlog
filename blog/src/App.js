12b86f7372c0fa0ee579a13cf159f0d2cfa64ba3
MY <fourp@naver.com>
2024-01-29 17:40:05 +0900

    상단 nav-bar 생성

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // {}으로 데이터 바인딩을 쉽게 할 수 있다.
+  // {}에 변수명, 함수등을 써주면 됨.
+  let title = '글 제목';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div> <img src={logo}/> React Blog :D </div>
+      </div>
+      <div className="list">
+        <h3>{title}</h3>
+        <p>2024.01.29</p>
+        <hr/>
+      </div>
     </div>
   );
 }
