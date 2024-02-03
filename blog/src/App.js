2bf7f88f940eaa64f680e49eaefd4822069336c1
MY <fourp@naver.com>
2024-02-04 08:16:06 +0900

    state array 변경 방법

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,13 +6,14 @@ function App() {
 
   let [title, titleUpdate] = useState(['블로그 제목1','블로그 제목2','블로그 제목 3','블로그 제목4']);
   let [likeUp, likeUpdate] = useState(0);
+  let [hateUp, hateUpdate] = useState(0);
   return (
     <div className="App">
       <div className="black-nav">
         <div> React Blog :D </div>
       </div>
       <div className="list">
-        <h3>{title[0]} <span onClick={ ()=>{ likeUpdate(likeUp + 1) } }>👍</span>{likeUp}</h3>
+        <h3>{title[0]} <span onClick={ ()=>{ likeUpdate(likeUp + 1) } }>👍</span>{likeUp} <span onClick={ ()=>{ hateUpdate(hateUp - 1) } }>👎</span>{hateUp}</h3>
         <p>2024.01.29</p>
         <hr/>
       </div>
